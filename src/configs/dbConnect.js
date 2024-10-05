ff842467e98b62027e4986594629931916e3bb8e
firas barghouth <barghouthf@gmail.com>
2024-10-04 22:45:11 -0900

    Test1

--- src/configs/dbConnect.js
+++ src/configs/dbConnect.js
@@ -7,8 +7,8 @@ export async function connectToDatabase() {
     'mongodb+srv://umanoinfo00:B4l0jIYvEdtOzNDE@umanosing.6seqopc.mongodb.net/UmanoDB?retryWrites=true&w=majority'
 
     // 'mongodb://127.0.0.1:27017/umano?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.4'
-     
-    test
+
+    // test
   )
 
   setTimeout(() => {
